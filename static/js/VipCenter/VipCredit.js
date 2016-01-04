0e9fbeb1b85abb5dca896e766c5eed0c96e8c256
eternaltreesnow <562276119@qq.com>
2016-01-04 11:47:46 +0800

    Add VipCredit profit reason & delete operation

--- static/js/VipCenter/VipCredit.js
+++ static/js/VipCenter/VipCredit.js
@@ -29,10 +29,6 @@ $(function() {
 
     $filterDate = $("#filterDate");
     $inputKeyword = $("#inputKeyword");
-    $checkId = $("#checkId");
-    $checkModal = $("#checkModal");
-    $passId = $("#passId");
-    $passModal = $("#passModal");
 
     var column = [
         {"data": "id"},
@@ -40,9 +36,8 @@ $(function() {
         {"data": "user.phone"},
         {"data": "points"},
         {"data": "time"},
-        {"data": "reason"},
-        {"data": "status"},
-        {"data": ""}
+        {"data": "profitreason"},
+        {"data": "status"}
     ];
     var tempdata = [
         {
@@ -58,68 +53,74 @@ $(function() {
         }
     ];
 
+    $.ajax({
+        url: "../../lib/ProfitReason.json",
+        type: "GET",
+        success: function(data) {
+            initDatatable(data);
+        },
+        error: function(data) {
+            console.log(data);
+        }
+    })
+
     /**
      * 初始化表格
      * $vipCreditTable 积分表格
      */
-    $vipCreditTable = $("#vipCreditTable");
-    datatable = $vipCreditTable.DataTable({
-        processing: true,
-        language: {
-            "search" : "内容搜索: ",
-            "searchPlaceholder" : "ID/手机号/...",
-            "processing": "数据加载中, 请稍后...",
-            "zeroRecords": "记录数为0...",
-            "emptyTable":  "记录数为0...",
-            "paginate": {
-                "first": "首页",
-                "previous": "上一页",
-                "next": "下一页",
-                "last": "尾页"
+    function initDatatable(reasonData) {
+        $vipCreditTable = $("#vipCreditTable");
+        datatable = $vipCreditTable.DataTable({
+            processing: true,
+            language: {
+                "search" : "内容搜索: ",
+                "searchPlaceholder" : "ID/手机号/...",
+                "processing": "数据加载中, 请稍后...",
+                "zeroRecords": "记录数为0...",
+                "emptyTable":  "记录数为0...",
+                "paginate": {
+                    "first": "首页",
+                    "previous": "上一页",
+                    "next": "下一页",
+                    "last": "尾页"
+                },
+                "lengthMenu": '每页显示 _MENU_ 条记录'
             },
-            "lengthMenu": '每页显示 _MENU_ 条记录'
-        },
-        columns: column,
-        pagingType: "full_numbers",
-        dom: 'rtlp',
-        serverSide: true,
-        ajax: {
-            url: '/_admin/s/user_profit_details',
-            type: 'GET',
-            data: function(d) {
-                delete d.columns;
-                delete d.order;
-                d.limit = d.length;
-                delete d.length;
-                d.search.time = moment($filterDate.val()).format('x');
-                d.search.keyword = $("#inputKeyword").val();
-                d.keyword = $("#inputKeyword").val();
-            },
-            dataSrc: function(json) {
-                for(var i=0; i<json.data.length; i++) {
-                    json.data[i]['time'] = moment(json.data[i]['time']).format('YYYY-MM-DD HH:mm:ss');
+            columns: column,
+            pagingType: "full_numbers",
+            dom: 'rtlp',
+            serverSide: true,
+            ajax: {
+                url: '/_admin/s/user_profit_details',
+                type: 'GET',
+                data: function(d) {
+                    delete d.columns;
+                    delete d.order;
+                    d.limit = d.length;
+                    delete d.length;
+                    d.search.time = moment($filterDate.val()).format('x');
+                    d.search.keyword = $("#inputKeyword").val();
+                    d.keyword = $("#inputKeyword").val();
+                },
+                dataSrc: function(json) {
+                    for(var i=0; i<json.data.length; i++) {
+                        json.data[i]['time'] = moment(json.data[i]['time']).format('YYYY-MM-DD HH:mm:ss');
+                        for(var j=0; j<reasonData.length; j++) {
+                            if(json.data[i]['reason'] == reasonData[j]['code']) {
+                                json.data[i]['profitreason'] = reasonData[j]['name'];
+                            }
+                        }
+                    }
+                    return json.data;
                 }
-                return json.data;
-            }
-        },
-        sortClasses: false,
-        columnDefs: [{
-            "targets" : -1,
-            "data" : null,
-            "defaultContent" : '<a href="javascript:void(0);" class="btn btn-primary btn-xs" data-link="check">审核</a>' +
-                               '<a href="javascript:void(0);" class="btn btn-success btn-xs" data-link="pass">通过</a>' +
-                               '<a href="javascript:void(0);" class="btn btn-default btn-xs" data-link="modify">修改</a>'
-        }],
-        drawCallback: function(settings, json) {
-            bindBtnEvent();
-        }
-    });
+            },
+            sortClasses: false
+        });
+    }
 
     $searchBtn = $("#searchBtn");
     $searchBtn.on('click', function() {
-        datatable.ajax.reload(function ( json ) {
-            bindBtnEvent();
-        });
+        datatable.ajax.reload();
     });
 
     $datetimepicker = $("#datetimepicker");
@@ -128,96 +129,4 @@ $(function() {
         format: 'YYYY-MM-DD'
     });
 
-    function bindBtnEvent() {
-        $linkCheck = $('[data-link="check"]');
-        $linkPass = $('[data-link="pass"]');
-        $linkModify = $('[data-link="modify"]');
-
-        $linkCheck.on('click', function() {
-            var $this = $(this);
-            $checkId.val($this.parents("tr").children(":first").html());
-            $checkModal.modal('show');
-        });
-
-        $linkPass.on('click', function() {
-            var $this = $(this);
-            $passId.val($this.parents("tr").children(":first").html());
-            $passModal.modal('show');
-        });
-
-        $linkModify.on('click', function() {
-            var $this = $(this);
-            var id = $this.parents("tr").children(":first").html();
-            location.href = "EditVipCredit.html?id=" + id;
-        });
-    }
-
-    $checkModalBtn = $("#checkModalBtn");
-    $checkModalBtn.on('click', function() {
-        var requestData = {
-            "id" : $checkId.val(),
-            "check" : true
-        };
-        $.ajax({
-            type: "POST",
-            url: "",
-            data: requestData,
-            dataType: "json",
-            success: function(data) {
-            },
-            error: function(data) {
-            }
-        });
-    });
-    $uncheckModalBtn = $("#uncheckModalBtn");
-    $uncheckModalBtn.on('click', function() {
-        var requestData = {
-            "id" : $checkId.val(),
-            "check" : false
-        };
-        $.ajax({
-            type: "POST",
-            url: "",
-            data: requestData,
-            dataType: "json",
-            success: function(data) {
-            },
-            error: function(data) {
-            }
-        });
-    });
-    $passModalBtn = $("#passModalBtn");
-    $passModalBtn.on('click', function() {
-        var requestData = {
-            "id" : $passId.val(),
-            "check" : true
-        };
-        $.ajax({
-            type: "POST",
-            url: "",
-            data: requestData,
-            dataType: "json",
-            success: function(data) {
-            },
-            error: function(data) {
-            }
-        });
-    });
-    $unpassModalBtn = $("#unpassModalBtn");
-    $unpassModalBtn.on('click', function() {
-        var requestData = {
-            "id" : $passId.val(),
-            "check" : false
-        };
-        $.ajax({
-            type: "POST",
-            url: "",
-            data: requestData,
-            dataType: "json",
-            success: function(data) {
-            },
-            error: function(data) {
-            }
-        });
-    });
 });
